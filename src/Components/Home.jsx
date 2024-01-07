a662d88785c3c1905ef75ccbce79dfb05137b438
mohan-246 <mohan5497306@gmail.com>
2024-01-07 18:54:40 +0530

    change loading logic

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,19 +5,13 @@ import Loader from "./Loader";
 const Home = () => {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
-    const handlePageLoad = () => {
-    //   console.log("All images are loaded.");
+    
     setTimeout(() => {
         setLoading(false)
     }, 100);
       
-    };
+   
 
-    window.addEventListener("load", handlePageLoad);
-
-    return () => {
-      window.removeEventListener("load", handlePageLoad);
-    };
   }, []);
   return <>
   {loading ? <Loader /> : <App />}
